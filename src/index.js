f21a34081f1a0ab236e2ae2b0468e480b778b0fa
Eduardo José Maya Rodriguez <36246464+edopore@users.noreply.github.com>
2024-02-14 16:36:54 -0500

    UPDATE image finder completed

---
+++ src/index.js
@@ -0,0 +1,116 @@
+import { fetchApiAsync } from "./pixabay-api";
+import { Notify } from "notiflix/build/notiflix-notify-aio";
+
+const inputForm = document.querySelector("input");
+const submitForm = document.querySelector("button");
+const imageGallery = document.querySelector(".gallery");
+
+const buttonMore = document.querySelector(".load-more");
+buttonMore.classList.add("btn-more--visible");
+
+let pageCounter = 1;
+
+submitForm.addEventListener("click", (event) => {
+  event.preventDefault();
+  pageCounter = 1;
+  imageGallery.innerHTML = "";
+  if (inputForm.value === "") {
+    return Notify.failure("Ingresa en el buscador la palabra clave");
+  }
+  const result = async (query) => {
+    try {
+      buttonMore.classList.remove("btn-more--visible");
+      const response = await fetchApiAsync(query);
+      const { total, totalHits, hits } = response.data;
+      if (totalHits === 0) {
+        buttonMore.classList.add("btn-more--visible");
+        Notify.failure(
+          "Sorry, there are no images matching your search query. Please try again."
+        );
+        return;
+      } else if (totalHits <= 40) {
+        buttonMore.classList.add("btn-more--visible");
+      }
+      Notify.success(`Hooray! We found ${totalHits} images.`);
+      hits.forEach((element) => {
+        imageGallery.innerHTML += `
+                <div class="photo-card">
+                    <img src="${element.largeImageURL}" alt="${element.pageURL}" loading="lazy" />
+                    <div class="info">
+                        <p class="info-item">
+                            <b>Likes</b>
+                            ${element.likes}
+                        </p>
+                        <p class="info-item">
+                            <b>Views</b>
+                            ${element.views}
+                        </p>
+                        <p class="info-item">
+                            <b>Comments</b>
+                            ${element.comments}
+                        </p>
+                        <p class="info-item">
+                            <b>Downloads</b>
+                            ${element.downloads}
+                        </p>
+                    </div>
+                </div>
+                `;
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  result(inputForm.value);
+});
+
+buttonMore.addEventListener("click", (event) => {
+  event.preventDefault();
+  pageCounter++;
+  const result = async (query, page) => {
+    try {
+      buttonMore.classList.remove("btn-more--visible");
+      const response = await fetchApiAsync(query, page);
+      const { total, totalHits, hits } = response.data;
+      if (totalHits === 0) {
+        buttonMore.classList.add("btn-more--visible");
+        return Notify.failure(
+          "Sorry, there are no images matching your search query. Please try again."
+        );
+      } else if (totalHits <= 40 || pageCounter >= Math.ceil(totalHits / 40)) {
+        buttonMore.classList.add("btn-more--visible");
+        Notify.warning(
+          "We're sorry, but you've reached the end of search results."
+        );
+      }
+      hits.forEach((element) => {
+        imageGallery.innerHTML += `
+                <div class="photo-card">
+                    <img src="${element.largeImageURL}" alt="${element.pageURL}" loading="lazy" />
+                    <div class="info">
+                        <p class="info-item">
+                            <b>Likes</b>
+                            ${element.likes}
+                        </p>
+                        <p class="info-item">
+                            <b>Views</b>
+                            ${element.views}
+                        </p>
+                        <p class="info-item">
+                            <b>Comments</b>
+                            ${element.comments}
+                        </p>
+                        <p class="info-item">
+                            <b>Downloads</b>
+                            ${element.downloads}
+                        </p>
+                    </div>
+                </div>
+                `;
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  result(inputForm.value, pageCounter);
+});
--- src/pixabay-api.js
+++ src/pixabay-api.js
@@ -1,1 +1,46 @@
 import axios from "axios";
+
+const API_KEY = "42343056-d970fc336103e47429fc1ac41";
+const query_options = {
+  imageType: "photo",
+  orientation: "horizontal",
+  safeSearch: "true",
+  pretty: "true",
+  perPage: 40,
+};
+const BASE_URL = `https://pixabay.com/api/`;
+function fetchApi(query) {
+  return axios
+    .get(
+      BASE_URL +
+        `?key=${API_KEY}&q=${query}
+    &image_type=${query_options.imageType}
+    &orientation=${query_options.orientation}
+    &safesearch=${query_options.safeSearch}
+    &pretty=${query_options.pretty}
+    &per_page=${query_options.perPage}`
+    )
+    .then((response) => response)
+    .then(({ data }) => console.log(data));
+}
+async function fetchApiAsync(query, page) {
+  const pageParam = page || 1;
+  try {
+    return await axios.get(
+      BASE_URL +
+        `?key=${API_KEY}&q=${query}
+            &image_type=${query_options.imageType}
+            &orientation=${query_options.orientation}
+            &safesearch=${query_options.safeSearch}
+            &pretty=${query_options.pretty}
+            &page=${pageParam}
+            &per_page=${query_options.perPage}`
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = {
+  fetchApiAsync,
+};
